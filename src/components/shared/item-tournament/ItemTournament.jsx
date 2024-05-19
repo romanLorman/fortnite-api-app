07a79878f39242d8461ea10b079eda59de8a12fd
romanLorman <148644864+romanLorman@users.noreply.github.com>
2024-05-19 20:47:18 +0300

    Update ItemTournament.jsx

--- src/components/shared/item-tournament/ItemTournament.jsx
+++ src/components/shared/item-tournament/ItemTournament.jsx
@@ -14,69 +14,72 @@ export const ItemTournament = ({ parentBlock, data }) => {
         >
           <rect x="0" y="0" rx="0" ry="0" width="390" height="561" />
         </ContentLoader>
-
-        <div className="item-tournament__content">
-          <div className="item-tournament__background-img">
-            <img
-              src={data.posterBack}
-              alt="tournament-background"
-              loading="lazy"
-            />
-          </div>
-          <div className="item-tournament__content-box">
-            <div className="item-tournament__head">
-              <div
-                className="item-tournament__title_large"
-                style={{ color: `#${data.renderData.title_color}` }}
-              >
-                {data.name_line1}
-              </div>
-              <div
-                className="item-tournament__label"
-                style={{ color: `#${data.renderData.shadow_color}` }}
-              >
-                <span>
-                  type:
-                  <div
-                    className="item-tournament__type"
-                    style={{ color: `#${data.renderData.primary_color}` }}
-                  >
-                    {data.name_line2}
-                  </div>
-                </span>
-
-                <span>
-                  schedule:
-                  <div
-                    className="item-tournament__date"
-                    style={{ color: `#${data.renderData.primary_color}` }}
-                  >
-                    {data.schedule}
-                  </div>
-                </span>
-              </div>
+        {data ? (
+          <div className="item-tournament__content">
+            <div className="item-tournament__background-img">
+              <img
+                src={data.posterBack}
+                alt="tournament-background"
+                loading="lazy"
+              />
             </div>
-            <div className="item-tournament__img">
-              <img src={data.tileImage} alt="poster" loading="lazy" />
-            </div>
-            <div className="item-tournament__body">
-              <div
-                className="item-tournament__text"
-                style={{ color: `#${data.renderData.primary_color}` }}
-              >
-                {
-                  <span
-                    style={{
-                      backgroundColor: `#${data.renderData.background_text_color}`,
-                    }}
-                  >
-                    “ {data.long_description} ”
+            <div className="item-tournament__content-box">
+              <div className="item-tournament__head">
+                <div
+                  className="item-tournament__title_large"
+                  style={{ color: `#${data.renderData.title_color}` }}
+                >
+                  {data.name_line1}
+                </div>
+                <div
+                  className="item-tournament__label"
+                  style={{ color: `#${data.renderData.shadow_color}` }}
+                >
+                  <span>
+                    type:
+                    <div
+                      className="item-tournament__type"
+                      style={{ color: `#${data.renderData.primary_color}` }}
+                    >
+                      {data.name_line2}
+                    </div>
                   </span>
-                }
+
+                  <span>
+                    schedule:
+                    <div
+                      className="item-tournament__date"
+                      style={{ color: `#${data.renderData.primary_color}` }}
+                    >
+                      {data.schedule}
+                    </div>
+                  </span>
+                </div>
+              </div>
+              <div className="item-tournament__img">
+                <img src={data.tileImage} alt="poster" loading="lazy" />
+              </div>
+              <div className="item-tournament__body">
+                <div
+                  className="item-tournament__text"
+                  style={{ color: `#${data.renderData.primary_color}` }}
+                >
+                  {
+                    <span
+                      style={{
+                        backgroundColor: `#${data.renderData.background_text_color}`,
+                      }}
+                    >
+                      “ {data.long_description} ”
+                    </span>
+                  }
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ) : (
+          ''
+        )}
       </div>
     </div>
   )
